28930285751e7685f63fa5a18378459730e029c8
pranto372 <prantodeb62@gmail.com>
2023-09-14 11:44:05 +0600

    Form

--- lib/main.dart
+++ lib/main.dart
@@ -17,42 +17,56 @@ class MyApp extends StatelessWidget{
  class HomeActivity extends StatelessWidget {
    HomeActivity({super.key});
 
-   MySnackBar(message, context){
-        return ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message))
-        );
-      }
-
-    MyAlertDialog(context){
-       return showDialog(
-       context: context,
-       builder: (BuildContext context){
-         return Expanded(
-             child: AlertDialog(
-               title: Text("Alert !"),
-               content: Text("Do you want to delete?"),
-               actions: [
-                 TextButton(onPressed: (){
-                   MySnackBar("Successfully delete", context);
-                   Navigator.of(context).pop();
-                 }, child: Text("Yes")),
-                 TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text("No")),
-               ],
-             )
-           );
-        }
-     );
-   }
    @override
    Widget build(BuildContext context) {
      return Scaffold(
        appBar: AppBar(
          title: Text("Practice"),
        ),
-        body: Center(
-         child: ElevatedButton(onPressed: (){MyAlertDialog(context);}, child: Text("Click me"),),
-        ),
+       body: Column(
+         mainAxisAlignment: MainAxisAlignment.start,
+         children: [
+           Padding(padding: EdgeInsets.all(10)),
+           TextField(
+             decoration: InputDecoration(border: OutlineInputBorder(),
+             labelText: "First name"
+             ),
+           ),
+           Padding(padding: EdgeInsets.all(10)),
+           TextField(
+             decoration: InputDecoration(border: OutlineInputBorder(),
+                 labelText: "Last name"
+             ),
+           ),
+           Padding(padding: EdgeInsets.all(10)),
+           TextField(
+             decoration: InputDecoration(border: OutlineInputBorder(),
+                 labelText: "Email"
+             ),
+           ),
+           Padding(padding: EdgeInsets.all(10)),
+           ElevatedButton(onPressed: (){}, child: Text("Submit")),
+         ],
+       ),
      );
    }
  }
+/// Form ***
+// body: Column(
+//   mainAxisAlignment: MainAxisAlignment.start,
+//   children: [
+//     Padding(padding: EdgeInsets.all(20),
+//     child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'First name' ),),
+//     ),
+//     Padding(padding: EdgeInsets.all(20),
+//       child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'Last name' ),),
+//     ),
+//     Padding(padding: EdgeInsets.all(20),
+//       child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'E-mail' ),),
+//     ),
+//     Padding(padding: EdgeInsets.all(20),
+//       child: ElevatedButton(onPressed: (){},child: Text("Submit"),),
+//     )
+//   ],
+// ),
 
