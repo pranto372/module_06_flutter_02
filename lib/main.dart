db362d2102360354ff8b982f0bb80a5bbd3f2cf9
pranto372 <prantodeb62@gmail.com>
2023-09-17 10:47:14 +0600

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,171 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeActivity(),
+      home: HomeScreen(),
     );
   }
 }
- class HomeActivity extends StatelessWidget {
-   HomeActivity({super.key});
+ class HomeScreen extends StatelessWidget {
+   HomeScreen({super.key});
 
    @override
    Widget build(BuildContext context) {
      return Scaffold(
        appBar: AppBar(
-         title: Text("Practice"),
+         title: Text("Home"),
        ),
-
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Text(
+              "Home",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+             ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingScreen()));
+                },
+                child: Text("Go to Settings")),
+          ],
+         ),
+       ),
      );
    }
  }
-/// Icon Class ***
-// body: Center(
-//   child: Icon(Icons.person, size: 50,color: Colors.purpleAccent,),
-// ),
 
-/// Linear Progress ***
-// body: Center(
-//   child: LinearProgressIndicator(
-//     minHeight: 10,
-//     color: Colors.purpleAccent,
-//     backgroundColor: Colors.black,
-//   ),
-// ),
+ /// Route
+class SettingScreen extends StatelessWidget {
+  SettingScreen({super.key});
 
-/// Circle Progress ***
-// body: Center(
-//   child: CircularProgressIndicator(
-//     color: Colors.black,
-//     strokeWidth: 5,
-//   ),
-// ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Setting"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Settings", style: TextStyle(fontSize: 20),),
+            SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(onPressed: (){
+              Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => OrderScreen()));
+                }, child: Text("Go to Orders")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Route
+class OrderScreen extends StatelessWidget {
+  OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Order"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Order", style: TextStyle(fontSize: 20),),
+            SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+            }, child: Text("Go to Home")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+///  Navigation ***
+// class HomeActivity extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home"),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: [
+//           SizedBox(
+//             height: 20,
+//           ),
+//           ElevatedButton(onPressed: (){
+//             Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from home to Activity 1")));
+//           },
+//               child: Text("Go Activity 1"),
+//           ),
+//           SizedBox(
+//             height: 20,
+//           ),
+//           ElevatedButton(onPressed: (){
+//             Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from home to Activity 2")));
+//           },
+//             child: Text("Go Activity 2"),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+//   class Activity1 extends StatelessWidget{
+//   String msg;
+//   Activity1(
+//       this.msg,
+//       {super.key}
+//       );
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(msg),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(onPressed: (){
+//           Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from Activity 1 to Activity 2")));
+//         },
+//         child: Text("Go Activity 2"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class Activity2 extends StatelessWidget{
+//   String msg;
+//   Activity2(
+//       this.msg,
+//       {super.key}
+//       );
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(msg),
+//       ),
+//     );
+//   }
+// }
 
 
 
