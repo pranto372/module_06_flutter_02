0d702a841a1465077e2312a40183023f5282a1b6
pranto372 <prantodeb62@gmail.com>
2023-09-18 09:53:52 +0600

    Module 6 Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,131 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
+void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home: const MyHomePage(),
     );
   }
 }
- class HomeScreen extends StatelessWidget {
-   HomeScreen({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         title: Text("Home"),
-       ),
-       body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text(
-              "Home",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SettingScreen()));
-                },
-                child: Text("Go to Settings")),
-          ],
-         ),
-       ),
-     );
-   }
- }
-
- /// Route
-class SettingScreen extends StatelessWidget {
-  SettingScreen({super.key});
-
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  static const List<String> imageLink = [
+    'https://cdn.pixabay.com/photo/2020/06/04/15/52/kitten-5259329_640.jpg',
+    'https://images.unsplash.com/photo-1583524505974-6facd53f4597?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8&w=1000&q=80',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMsK02fjWYL2tOrzp4LjdycEBGiVJW3OP73oC9Qf89mGh-8EsmvyBGpOu410lin-x6hjE&usqp=CAU',
+    'https://images.unsplash.com/photo-1638826597195-2413862ccaa3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+    'https://images.unsplash.com/photo-1619043544947-7d7fc328cacb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80',
+    'https://images.unsplash.com/photo-1571501515601-5ec2da8b9f35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Setting"),
+        title: const Text('Photo Gallery'),
+        centerTitle: true,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Settings", style: TextStyle(fontSize: 20),),
-            SizedBox(
-              height: 10,
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
             ),
-            OutlinedButton(onPressed: (){
-              Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderScreen()));
-                }, child: Text("Go to Orders")),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for photos...',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: imageLink.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 30,
+                    crossAxisSpacing: 30,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GridTile(
+                      footer: Container(color: Colors.white,child: Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Center(child: Text("Photo $index",style: const TextStyle(fontSize: 20),)),
+                      ),),
+                      child: GestureDetector(
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Image $index clicked!'),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          height: 150,
+                          color: Colors.grey,
+                          child: Image.network(
+                            imageLink[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              child: ListView.builder(
+                itemCount: 3,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder:  (context, iindex) =>  ListTile(
+                  leading: CircleAvatar(backgroundImage:NetworkImage(imageLink[iindex]),),
+                  title: Text('Photo ${iindex+1}'),
+                  subtitle: Text('Description of photo ${iindex+1}'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            FloatingActionButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Photos Uploaded Successfully!'),
+                  ),
+                );
+              },
+              child: const Icon(Icons.upload),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-/// Route
-class OrderScreen extends StatelessWidget {
-  OrderScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Order"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Order", style: TextStyle(fontSize: 20),),
-            SizedBox(
-              height: 10,
-            ),
-            OutlinedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
-            }, child: Text("Go to Home")),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-///  Navigation ***
-// class HomeActivity extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Home"),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         children: [
-//           SizedBox(
-//             height: 20,
-//           ),
-//           ElevatedButton(onPressed: (){
-//             Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1("This is from home to Activity 1")));
-//           },
-//               child: Text("Go Activity 1"),
-//           ),
-//           SizedBox(
-//             height: 20,
-//           ),
-//           ElevatedButton(onPressed: (){
-//             Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from home to Activity 2")));
-//           },
-//             child: Text("Go Activity 2"),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-//
-//   class Activity1 extends StatelessWidget{
-//   String msg;
-//   Activity1(
-//       this.msg,
-//       {super.key}
-//       );
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(msg),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(onPressed: (){
-//           Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2("This is from Activity 1 to Activity 2")));
-//         },
-//         child: Text("Go Activity 2"),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class Activity2 extends StatelessWidget{
-//   String msg;
-//   Activity2(
-//       this.msg,
-//       {super.key}
-//       );
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(msg),
-//       ),
-//     );
-//   }
-// }
-
-
-
