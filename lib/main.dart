2e22c16cfe7a9ed6a428d73953c86955fb746898
pranto372 <prantodeb62@gmail.com>
2023-09-19 12:31:13 +0600

    Grid View

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(),
     );
   }
@@ -22,6 +20,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
   static const List<String> imageLink = [
     'https://cdn.pixabay.com/photo/2020/06/04/15/52/kitten-5259329_640.jpg',
     'https://images.unsplash.com/photo-1583524505974-6facd53f4597?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8&w=1000&q=80',
@@ -30,102 +29,130 @@ class MyHomePage extends StatelessWidget {
     'https://images.unsplash.com/photo-1619043544947-7d7fc328cacb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80',
     'https://images.unsplash.com/photo-1571501515601-5ec2da8b9f35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&w=1000&q=80'
   ];
+
   @override
   Widget build(BuildContext context) {
+
+    // var colors = [
+    //   Colors.purpleAccent,
+    //   Colors.red,
+    //   Colors.black,
+    //   Colors.blue,
+    //   Colors.orange,
+    //   Colors.amber,
+    //   Colors.blueGrey
+    // ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Photo Gallery'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Welcome to My Photo Gallery!',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold
+      body: SizedBox(
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage(imageLink[index]),
+                    fit: BoxFit.cover
+                  )
                 ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search for photos...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            SizedBox(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: imageLink.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 30,
-                    crossAxisSpacing: 30,
-                  ),
-                  itemBuilder: (context, index) {
-                    return GridTile(
-                      footer: Container(color: Colors.white,child: Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Center(child: Text("Photo $index",style: const TextStyle(fontSize: 20),)),
-                      ),),
-                      child: GestureDetector(
-                        onTap: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Image $index clicked!'),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          height: 150,
-                          color: Colors.grey,
-                          child: Image.network(
-                            imageLink[index],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            SizedBox(
-              child: ListView.builder(
-                itemCount: 3,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder:  (context, iindex) =>  ListTile(
-                  leading: CircleAvatar(backgroundImage:NetworkImage(imageLink[iindex]),),
-                  title: Text('Photo ${iindex+1}'),
-                  subtitle: Text('Description of photo ${iindex+1}'),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10,),
-            FloatingActionButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Photos Uploaded Successfully!'),
-                  ),
-                );
-              },
-              child: const Icon(Icons.upload),
-            ),
-          ],
+              );
+            },itemCount: imageLink.length,
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+      //       SizedBox(
+      //         child: Padding(
+      //           padding: const EdgeInsets.all(16.0),
+      //           child: GridView.builder(
+      //             physics: const NeverScrollableScrollPhysics(),
+      //             shrinkWrap: true,
+      //             itemCount: imageLink.length,
+      //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //               crossAxisCount: 3,
+      //               mainAxisSpacing: 30,
+      //               crossAxisSpacing: 30,
+      //             ),
+      //             itemBuilder: (context, index) {
+      //               return GridTile(
+      //                 footer: Container(
+      //                   color: Colors.white,
+      //                   child: Padding(
+      //                     padding: const EdgeInsets.only(top: 10),
+      //                     child: Center(
+      //                         child: Text(
+      //                       "Photo $index",
+      //                       style: const TextStyle(fontSize: 20),
+      //                     )),
+      //                   ),
+      //                 ),
+      //                 child: GestureDetector(
+      //                   onTap: () {
+      //                     ScaffoldMessenger.of(context).showSnackBar(
+      //                       SnackBar(
+      //                         content: Text('Image $index clicked!'),
+      //                       ),
+      //                     );
+      //                   },
+      //                   child: Container(
+      //                     height: 150,
+      //                     color: Colors.grey,
+      //                     child: Image.network(
+      //                       imageLink[index],
+      //                       fit: BoxFit.cover,
+      //                     ),
+      //                   ),
+      //                 ),
+      //               );
+      //             },
+      //           ),
+      //         ),
+      //       ),
+      //       SizedBox(
+      //         child: ListView.builder(
+      //           itemCount: 3,
+      //           shrinkWrap: true,
+      //           physics: const NeverScrollableScrollPhysics(),
+      //           itemBuilder: (context, iindex) => ListTile(
+      //             leading: CircleAvatar(
+      //               backgroundImage: NetworkImage(imageLink[iindex]),
+      //             ),
+      //             title: Text('Photo ${iindex + 1}'),
+      //             subtitle: Text('Description of photo ${iindex + 1}'),
+      //           ),
+      //         ),
+      //       ),
+      //       const SizedBox(
+      //         height: 10,
+      //       ),
+      //       FloatingActionButton(
+      //         onPressed: () {
+      //           ScaffoldMessenger.of(context).showSnackBar(
+      //             const SnackBar(
+      //               content: Text('Photos Uploaded Successfully!'),
+      //             ),
+      //           );
+      //         },
+      //         child: const Icon(Icons.upload),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
